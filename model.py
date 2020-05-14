bc9d2c527cf86d82f4b1afb7743e3b3ece6a3205
emilyedotb <65319247+emilyedotb@users.noreply.github.com>
2020-05-14 19:57:15 -0400

    Random order dealing

--- model.py
+++ model.py
@@ -1,4 +1,23 @@
-def main():
-    print("hello, world")
+import random
+# deal cards to beginning players
+# 2-4 players; each player gets a deck of cards
 
-main()
+class Card:
+    def __init__(self, color, city_name):
+        self.color = color
+        self.city_name = city_name
+
+class Deck:
+    def __init__(self):
+        self.cards = [Card("blue", "Chennai"), Card("yellow", "San Francisco"), Card("black", "London")]
+        # pick a random number to establish deck order
+        # e.g. a list of randomly scrambled non repeating numbers
+        self.deck_order = []
+        for i in range(48):
+            num = random.randint(0,48)
+            while num in self.deck_order:
+                num = random.randint(0,48)
+            self.deck_order.append(num)
+
+d = Deck()
+print(d.deck_order)
