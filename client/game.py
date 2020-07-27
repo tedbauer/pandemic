6425123ba7d955cbdcad48dbe3f36eb10b07f079
tedbauer <tjb272@cornell.edu>
2020-07-27 16:45:03 -0400

    Add initial mouse navigation to game board. Fixes #3

--- client/game.py
+++ client/game.py
@@ -2,6 +2,7 @@ import random
 
 import pygame
 from pygame.sprite import Group
+from pygame import Rect
 
 from client.board import City
 from client.scene import Scene
@@ -18,6 +19,39 @@ color_map = {
     "black": BLACK,
 }
 
+class CameraGroup(Group):
+
+    def __init__(self):
+        Group.__init__(self)
+        self.x = 0
+        self.y = 0
+
+        self.upper_box = Rect(0, 0, 1000, 150)
+        self.lower_box = Rect(0, 650, 1000, 150)
+        self.left_box = Rect(0, 0, 150, 800)
+        self.right_box = Rect(850, 0, 150, 800)
+        self.camera_speed = 5
+
+
+    def draw(self, surface):
+        #TODO: the overridden draw() might have useful functionality that we're missing
+        for sprite in self.sprites():
+            surface.blit(sprite.image, (sprite.rect.x + self.x, sprite.rect.y + self.y))
+
+
+    def update(self, server_state, events):
+        Group.update(self, server_state, events)
+
+        if self.upper_box.collidepoint(pygame.mouse.get_pos()):
+            self.y -= self.camera_speed
+        if self.lower_box.collidepoint(pygame.mouse.get_pos()):
+            self.y += self.camera_speed
+        if self.left_box.collidepoint(pygame.mouse.get_pos()):
+            self.x -= self.camera_speed
+        if self.right_box.collidepoint(pygame.mouse.get_pos()):
+            self.x += self.camera_speed
+
+
 class Game(Scene):
 
     def __init__(self, screen, channel, initial_state):
@@ -26,7 +60,7 @@ class Game(Scene):
         self.screen = screen
         self.channel = channel
 
-        city_group = Group()
+        city_group = CameraGroup()
         for city_node in initial_state.city_nodes:
             x, y = random.randint(0, 600), random.randint(0, 600)
             city_group.add(City(x, y, color_map[city_node.color.lower()], city_node.city_name, None))
